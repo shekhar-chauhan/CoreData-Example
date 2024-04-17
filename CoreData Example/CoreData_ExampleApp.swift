2028e152f44bc42b9bd73e9a6cfe2a65d7dff934
Shekhar Chauhan <shekharbarca10@gmail.com>
2024-04-17 15:56:29 +0530

    Initial Commit

---
+++ CoreData Example/CoreData_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreData_ExampleApp.swift
+//  CoreData Example
+//
+//  Created by Shekhar Chauhan on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct CoreData_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
